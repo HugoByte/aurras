5f23cfe0f69b43dffd60827130becc0c32a4353f
Muhammed Irfan K <muhammedirfan@hugobyte.com>
2021-02-05 15:06:22 +0530

    Add Event Receiver Action bolierplate

---
+++ actions/event-receiver/commands/banner.ts
@@ -0,0 +1,13 @@
+import figlet from 'figlet';
+import chalk from 'chalk';
+
+
+figlet(process.argv[2], (error: any, data: any) => {
+  if (error) {
+    return process.exit(1);
+  }
+
+  console.log(chalk.blue(data));
+  console.log('');
+  return process.exit(0);
+});
