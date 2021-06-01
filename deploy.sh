400c5dae3f24cf55c6596f10b7e11ca72e223686
Muhammed Irfan K <muhammedirfan@hugobyte.com>
2021-06-01 15:56:58 +0530

    Add event registration to deploy

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ openwhiskApiHost=${openwhiskApiHost:-https://localhost:31001}
 openwhiskApiKey=${openwhiskApiKey:-23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP}
 openwhiskNamespace=${openwhiskNamespace:-guest}
 
-actions=("actions/event-receiver")
+actions=("actions/event-receiver", "actions/event-registration")
 
 while [ $# -gt 0 ]; do
     if [[ $1 == *"--"* ]]; then
