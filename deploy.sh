e54e81ab44745e4e0b0e9db99b9734d840d6fc0a
Muhammed Irfan K <muhammedirfan@hugobyte.com>
2021-08-26 10:22:00 +0530

    Add to workspace

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,8 @@ openwhiskApiHost=${openwhiskApiHost:-https://localhost:31001}
 openwhiskApiKey=${openwhiskApiKey:-23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP}
 openwhiskNamespace=${openwhiskNamespace:-guest}
 
-actions=("actions/event-receiver" "actions/event-registration" "actions/notification")
+actions=("actions/push-notification" "actions/balance-filter" "actions/event-receiver" "actions/event-registration" "actions/balance-notification-registration" "actions/event-producer" "actions/kafka-provider-feed" "actions/kafka-provider-web" "actions/substrate-event-processor")
+
 while [ $# -gt 0 ]; do
     if [[ $1 == *"--"* ]]; then
         param="${1/--/}"
@@ -22,5 +23,5 @@ set -e
 for index in ${!actions[@]};
 do  
     chmod +x "$PWD/${actions[$index]}/deploy.sh"
-    "$PWD/${actions[$index]}/deploy.sh" --openwhiskApiHost "$openwhiskApiHost" --openwhiskApiKey "$openwhiskApiKey" --openwhiskNamespace "openwhiskNamespace"
+    "$PWD/${actions[$index]}/deploy.sh" --openwhiskApiHost "$openwhiskApiHost" --openwhiskApiKey "$openwhiskApiKey" --openwhiskNamespace "$openwhiskNamespace"
 done
