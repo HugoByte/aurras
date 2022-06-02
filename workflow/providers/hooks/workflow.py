974da55bd8e2cf0bacfb704e2c8d48cc7f127bb9
Shreyas K S <shreyas.ks@hugobyte.com>
2022-06-02 19:06:59 +0530

    update naming standards

--- workflow/providers/hooks/workflow.py
+++ workflow/providers/hooks/workflow.py
@@ -6,10 +6,10 @@ from typing import Any, List
 import copy
 
 # List to store flows from parsed config
-flows = []
+flow_list = []
 
 # List to store tasks from parsed config
-tasks = []
+task_list = []
 
 task_store = dict()
 
@@ -28,7 +28,7 @@ class Task(BaseHook):
     endpoint: str = Field(..., title='endpoint', description="Task Endpoint")
 
     def exec(self):
-        global tasks
+        global task_list
         task = {
             "task_name": self.name,
             "kind": self.kind,
@@ -37,20 +37,20 @@ class Task(BaseHook):
             "output_args": self.output_args
         }
 
-        tasks.append(task)
+        task_list.append(task)
 
         return
 
 
 # functions related to task hook for creating rust code
 
-task_struct = f"""use super::*;
+task_struct_impl = f"""use super::*;
 
 """
 impl_task_trait = "trait_impl_task!("
 impl_get_task_trait = "trait_impl_getting_task_name!("
 
-run = f"""
+implement_run_method = f"""
 
     fn run(&self) -> Value { 
         
@@ -66,33 +66,25 @@ task_store_copy = dict()
 impl_stucture = ""
 setter = ""
 new_method = ""
-action_props = dict()
+action_properties = dict()
 
-getter = f"""
+implement_get_action_name_method = f"""
 fn get_action_name(&self) -> String {
     {
         "self.action_name.clone()"
     }}
 """
 
-def convert_to_pascalcase(string: str)-> str:
+
+def convert_to_pascalcase(string: str) -> str:
 
     return string.replace("_", " ").title().replace(" ", "")
 
 
 def struct_generator():
-    global tasks
+    global task_list,impl_task_trait,impl_get_task_trait,task_struct_impl,impl_stucture,run,new_method,setter,task_store
 
-    global impl_task_trait
-    global impl_get_task_trait
-    global task_struct
-    global impl_stucture
-    global run
-    global new_method
-    global setter
-    global task_store
-
-    for task in tasks:
+    for task in task_list:
         task_dic = dict()
         name = task['task_name']
         kind = task['kind']
@@ -106,8 +98,8 @@ def struct_generator():
 
 impl {task_name} {{
         
-        {run}
-        {getter}
+        {implement_run_method}
+        {implement_get_action_name_method}
 }}
 
 
@@ -128,26 +120,26 @@ impl {task_name} {{
     impl_task_trait = impl_task_trait[:-1]+");"
     impl_get_task_trait = impl_get_task_trait[:-1]+");"
 
-    task_struct += impl_get_task_trait
-    task_struct += impl_task_trait
+    task_struct_impl += impl_get_task_trait
+    task_struct_impl += impl_task_trait
 
-    task_struct += impl_stucture.replace("'", "")
+    task_struct_impl += impl_stucture.replace("'", "")
     return
 
 
 def create_sturct(task_name: str, args: list, type: str, kind):
-    global task_struct
+    global task_struct_impl
     global impl_task_trait
-    global action_props
+    global action_properties
     action_prop = ""
 
-    for prop in action_props['action']:
-        if convert_to_pascalcase(prop['name']) == task_name:
+    for property in action_properties['action']:
+        if convert_to_pascalcase(property['name']) == task_name:
             action_prop += f"""
-#[AuthKey="{prop['auth_token']}"]
-#[ApiHost="{prop['api_host']}"]
-#[Insecure="{prop['insecure'].lower()}"]
-#[Namespace="{prop['namespace']}"]
+#[AuthKey="{property['auth_token']}"]
+#[ApiHost="{property['api_host']}"]
+#[Insecure="{property['insecure'].lower()}"]
+#[Namespace="{property['namespace']}"]
             """
 
     argument = ""
@@ -162,7 +154,7 @@ def create_sturct(task_name: str, args: list, type: str, kind):
             """
 
     if type == "input":
-        task_struct += f"""
+        task_struct_impl += f"""
 
 #[derive(Default, Debug, Clone, Serialize, Deserialize,{kind})]
 {action_prop}
@@ -173,15 +165,15 @@ pub struct {task_name}{{
     """
 
     if argument != "":
-        task_struct += f"""
+        task_struct_impl += f"""
 #[derive(Default, Debug, Clone, Serialize, Deserialize)]
 pub struct {task_name}{type.title()} {{
 {argument} 
 }}
 """
 
-    task_struct = task_struct.replace("\\", "")
-    task_struct = task_struct.rjust(len(task_struct))
+    task_struct_impl = task_struct_impl.replace("\\", "")
+    task_struct_impl = task_struct_impl.rjust(len(task_struct_impl))
 
     return
 
@@ -203,7 +195,7 @@ class Flow(BaseHook):
             "depends_on": self.depends_on,
         }
 
-        flows.append(flow)
+        flow_list.append(flow)
 
         return
 
@@ -212,14 +204,7 @@ dependency_matrix = dict()
 
 
 def create_main_input_struct():
-    global flows
-    global tasks
-    global task_store
-    global task_struct
-    global new_method
-    global generic_input_sturct_filed
-    global task_store_copy
-    global dependency_matrix
+    global flow_list, task_list, task_store, task_struct_impl, new_method, generic_input_sturct_filed, task_store_copy, dependency_matrix
     task_store_copy = copy.deepcopy(task_store)
     setter = dict()
     dependency = dict()
@@ -233,7 +218,7 @@ def create_main_input_struct():
     local_pipe_dic = dict()
     local_pipe_list = []
 
-    for flow in flows:
+    for flow in flow_list:
 
         if flow['type'] == "Init":
 
@@ -296,7 +281,7 @@ def create_main_input_struct():
                 depends_on_task = item['name'].replace(
                     "_", " ").title().replace(" ", "")
 
-                for task in tasks:
+                for task in task_list:
                     if map_task_name == convert_to_pascalcase(task['task_name']):
                         for i in task['output_args']:
                             current_task_type_map = i['type']
@@ -307,7 +292,7 @@ def create_main_input_struct():
                 if depend_task_type_map == "Vec<String>":
                     depend_task_type_map = "String"
 
-            task_struct += f"""
+            task_struct_impl += f"""
 #[derive(Default, Debug, Clone, Serialize, Deserialize)]
 pub struct {map_task_name }MapOutput{{
     model_prize_list : HashMap<{depend_task_type_map},{current_task_type_map}>
@@ -318,21 +303,21 @@ pub struct {map_task_name }MapOutput{{
         # dependency_matrix[flow['type']] = local_pipe_list
         local_pipe_list = []
 
-    task_struct += f"""
+    task_struct_impl += f"""
 #[derive(Default, Debug, Clone, Serialize, Deserialize)]
 pub struct Input{{
     {generic_input_sturct_filed.replace("'", "").replace("{", "").replace("}", "")}
 }}
 """
 
-    implement_new_and_setter(tasks, task_store_copy, setter, dependency)
+    implement_new_and_setter(task_list, task_store_copy, setter, dependency)
     return
 
 
-def implement_new_and_setter(tasks, task_store_copy, setter, dependency):
-    global task_struct
+def implement_new_and_setter(task_list, task_store_copy, setter, dependency):
+    global task_struct_impl
 
-    for task in tasks:
+    for task in task_list:
         task_name = convert_to_pascalcase(task['task_name'])
         method_param = f"{task_store_copy[task_name]}".replace(
             "'", "").replace("{", "").replace("}", "")
@@ -352,7 +337,8 @@ def implement_new_and_setter(tasks, task_store_copy, setter, dependency):
                 set_fileds = ""
                 for x in fields:
                     set_fileds += f"""self.input.{x} = value.{x};"""
-            dep_task_out = convert_to_pascalcase(" ".join(dependency[task_name].keys()))
+            dep_task_out = convert_to_pascalcase(
+                " ".join(dependency[task_name].keys()))
             setter_method += f"""fn setter(&mut self,value:{dep_task_out}Output){{{set_fileds}}}""".replace(
                 "\\n", "").replace("'", "")
             new_impl += f"""
@@ -363,7 +349,7 @@ impl {task_name} {{
 
 }}
 """
-            task_struct += new_impl.replace("'", "")
+            task_struct_impl += new_impl.replace("'", "")
         else:
             setter_method += "fn setter(&mut self){}"
             new_impl += f"""
@@ -374,16 +360,14 @@ impl {task_name} {{
 
 }}
 """
-            task_struct += new_impl.replace("'", "")
+            task_struct_impl += new_impl.replace("'", "")
 
     return
 
+main_file = ""
 
-def create_main_function(task) -> str:
-    global task_store
-    global task_store_copy
-    global dependency_matrix
-    global task_struct
+def create_main_function(task):
+    global task_store, task_store_copy, dependency_matrix, task_struct_impl, global_imports,main_file
     main = ""
     flow = ""
     dep_matrix = ""
@@ -427,23 +411,7 @@ let mut {k.lower()}_list : Flow<{k},{convert_to_pascalcase("".join(v))}Output> =
 """
 
     main += f"""
-mod common;
-mod traits;
-mod types;
-use common::*;
-use openwhisk_macro::OpenWhisk;
-use openwhisk_rust::*;
-use paste::paste;
-use std::fmt::Debug;
-use traits::*;
-use types::*;
-
-use serde::{{
-    Deserialize, Serialize,
-}};
-
-use serde_json::{{Error, Value}};
-use std::collections::HashMap;
+    {global_imports}    
 
     pub fn main(args:Value) -> Result<Value,Error>{{
     let input: Input = serde_json::from_value(args)?;
@@ -456,47 +424,50 @@ use std::collections::HashMap;
     Ok(result.unwrap())
 }}
     """
-    task_struct += setter_trait
-    return main
-
+    task_struct_impl += setter_trait
+    main_file += main
+    
 class WorkFlow(BaseHook):
     hook_type: str = 'workflow'
     name: str = Field(..., title='name', description='Workflow name')
     version: str = Field(..., title='version', description='Wrokflow Version')
-    action_properties : Any = Field(...,title="action_properties",description="Porperties for the actions defined")
+    action_properties: Any = Field(..., title="action_properties",
+                                   description="Porperties for the actions defined")
 
     def exec(self):
-        global tasks
+        global task_list
         global task_store_copy
-        global action_props
+        global action_properties
 
-        action_props = self.action_properties
+        action_properties = self.action_properties
         struct_generator()
         create_main_input_struct()
-        main_file_input = create_main_function(tasks)
-        output(main_file_input)
+        create_main_function(task_list)
+        generate_output()
 
-        
         return
 
-def output(main_file):
-    global cargo_file,common_rs_file,traits_file,task_struct
+
+def generate_output():
+    global cargo_file, common_rs_file, traits_file, task_struct_impl,main_file
     output_path = "../../"
-    path = os.path.join(output_path,"output/src")
-    os.makedirs(path,mode=0o777)
-    cargo = open(os.path.join(output_path,"output/Cargo.toml"),'w')
+    path = os.path.join(output_path, "output/src")
+    os.makedirs(path, mode=0o777)
+    cargo = open(os.path.join(output_path, "output/Cargo.toml"), 'w')
     cargo.writelines(cargo_file)
     cargo.close()
-    rustfile = open(os.path.join(output_path,"output/src/common.rs"),'w')
+
+    
+    rustfile = open(os.path.join(output_path, "output/src/common.rs"), 'w')
     rustfile.writelines(common_rs_file)
     rustfile.close()
-    rustfile = open(os.path.join(output_path,"output/src/traits.rs"),'w')
+    rustfile = open(os.path.join(output_path, "output/src/traits.rs"), 'w')
     rustfile.writelines(traits_file)
     rustfile.close()
-    rustfile = open(os.path.join(output_path,"output/src/types.rs"),'w')
-    rustfile.writelines(task_struct)
+    rustfile = open(os.path.join(output_path, "output/src/types.rs"), 'w')
+    rustfile.writelines(task_struct_impl)
     rustfile.close()
-    rustfile = open(os.path.join(output_path,"output/src/lib.rs"),'w')
+    rustfile = open(os.path.join(output_path, "output/src/lib.rs"), 'w')
     rustfile.writelines(main_file)
     rustfile.close()
 
@@ -668,3 +639,31 @@ pub trait Getter {{
     fn get_action_name(&self) -> String;
 }}
 """
+
+global_imports = f"""
+mod common;
+mod traits;
+mod types;
+use common::*;
+use openwhisk_macro::OpenWhisk;
+use openwhisk_rust::*;
+use paste::paste;
+use std::fmt::Debug;
+use traits::*;
+use types::*;
+
+use serde::{{
+    Deserialize, Serialize,
+}};
+
+use serde_json::{{Error, Value}};
+use std::collections::HashMap;
+"""
+
+files = [{
+    "common.rs" : common_rs_file,
+    "trait.rs" : traits_file,
+    "lib.rs" : main_file,
+    "types.rs" : task_struct_impl
+
+}]
