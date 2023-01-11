9e7dbebdc6c418f5c415b6c2ac22a757f6fe8187
Muhammed Irfan <muhammedirfan@hugobyte.com>
2023-01-11 12:58:57 +0530

    fix minor typo

--- workflow/composer/cmd/generate.go
+++ workflow/composer/cmd/generate.go
@@ -17,7 +17,7 @@ const ProviderPath = "../providers"
 // generateCmd represents the generate command
 var generateCmd = &cobra.Command{
 	Use:   "generate",
-	Short: "Geneates which uses hooks and configuration file to generate wasm binary out of it.",
+	Short: "Generates which uses hooks and configuration file to generate wasm binary out of it.",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 
