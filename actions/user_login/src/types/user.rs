d049a73b0c6313ab10a026830ac155e46a620aeb
Shanith K K <shanith.kk@hugobyte.com>
2023-05-23 19:54:20 +0530

    chore: Add user login action

---
+++ actions/user_login/src/types/user.rs
@@ -0,0 +1,32 @@
+use serde_derive::{Deserialize, Serialize};
+
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct User {
+    name: String,
+    email: String,
+    password: String,
+}
+
+#[allow(dead_code)]
+impl User {
+    pub fn new(name: String, email: String, password: String) -> Self {
+        User {
+            name,
+            email,
+            password,
+        }
+    }
+    pub fn get_password(&self) -> &String {
+        &self.password
+    }
+    pub fn get_name(&self) -> &String {
+        &self.name
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Claims {
+    pub sub: String,
+    pub exp: i64,
+}
