c33f137ce56cc1f95128142c5c572dd843f932af
Prathiksha-Nataraja <90592522+Prathiksha-Nataraja@users.noreply.github.com>
2023-09-13 17:39:58 +0530

    chore: frontend for getting and storing user input

--- examples/icon-eth-notification/src/pages/main_page.js
+++ examples/icon-eth-notification/src/pages/main_page.js
@@ -14,6 +14,13 @@ import {
   Label,
 } from "semantic-ui-react";
 import { useNavigate } from "react-router-dom";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 function Main() {
   const location = useLocation();
@@ -21,7 +28,7 @@ function Main() {
   // const user_auth_token = location.state.auth_token;
   const [auther, setAuth] = useState(localStorage.getItem("authToken"));
 
-  const [activeScreen, setActiveScreen] = useState("home");
+  const [activeScreen, setActiveScreen] = useState("home", "table");
 
   function handleChangeActiveScreen(screen) {
     setActiveScreen(screen);
@@ -38,6 +45,45 @@ function Main() {
   const handleAbout = () => {
     activeScreen === "home"
   };
+  const [inputText, setInputText] = useState("");
+  const [outputText, setOutputText] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setOutputText(inputText);
+    setInputText("");
+  };
+
+  const data = [
+    {
+      method: "Event1",
+      from: "From",
+      to: "To",
+      hash: "Hash",
+      timestamp: "Timestamp",
+    },
+    {
+      method: "Event2",
+      from: "From",
+      to: "To",
+      hash: "Hash",
+      timestamp: "Timestamp",
+    },
+  ];
+
+  function createData(method, from, to, hash, timestamp) {
+    return { method, from, to, hash, timestamp };
+  }
+
+  const rows = [
+    createData("method2", "from2", "to2", "hash2", "time2"),
+    createData("method3", "from3", "to3", "hash3", "time3"),
+  ];
+
   if (auther == "") {
     // console.log("please login");
     alert(" Please Login");
@@ -85,48 +131,141 @@ function Main() {
         <h1>Aurras</h1>
       </div>
 
-      {activeScreen === "home" && (
-        <div className="one">
-          <h1></h1>
-          <p>
-            Aurras is a middleware that acts as an event processor and a low
-            code workflow orchestration platform. Aurras is being pitched as a
-            next-generation system for enabling decentralized push notification.
-            This middleware solution listens to events from blockchain
-            applications and propagates them to a registered pool of MQTT
-            brokers. The broader architecture consist of parachain from which
-            the middleware listens for the events.
-          </p>
-          <p>
-            Aurras is a middleware that acts as an event processor and a low
-            code workflow orchestration platform. Aurras is being pitched as a
-            next-generation system for enabling decentralized push notification.
-            This middleware solution listens to events from blockchain
-            applications and propagates them to a registered pool of MQTT
-            brokers. The broader architecture consist of parachain from which
-            the middleware listens for the events.
-          </p>
+      {/* {activeScreen === "home" && ( */}
+      <div className="one">
+        <h1></h1>
+        <p>
+          Aurras is a middleware that acts as an event processor and a low code
+          workflow orchestration platform. Aurras is being pitched as a
+          next-generation system for enabling decentralized push notification.
+          This middleware solution listens to events from blockchain
+          applications and propagates them to a registered pool of MQTT brokers.
+          The broader architecture consist of parachain from which the
+          middleware listens for the events.
+        </p>
+        <p>
+          Aurras is a middleware that acts as an event processor and a low code
+          workflow orchestration platform. Aurras is being pitched as a
+          next-generation system for enabling decentralized push notification.
+          This middleware solution listens to events from blockchain
+          applications and propagates them to a registered pool of MQTT brokers.
+          The broader architecture consist of parachain from which the
+          middleware listens for the events.
+        </p>
 
-          <p>
-            Aurras is a middleware that acts as an event processor and a low
-            code workflow orchestration platform. Aurras is being pitched as a
-            next-generation system for enabling decentralized push notification.
-            This middleware solution listens to events from blockchain
-            applications and propagates them to a registered pool of MQTT
-            brokers. The broader architecture consist of parachain from which
-            the middleware listens for the events.
-          </p>
-        </div>
-      )}
+        <p>
+          Aurras is a middleware that acts as an event processor and a low code
+          workflow orchestration platform. Aurras is being pitched as a
+          next-generation system for enabling decentralized push notification.
+          This middleware solution listens to events from blockchain
+          applications and propagates them to a registered pool of MQTT brokers.
+          The broader architecture consist of parachain from which the
+          middleware listens for the events.
+        </p>
 
-      {/* {activeScreen === "table" && <table>table</table>}
-      <button
-        onClick={() => {
-          handleChangeActiveScreen("table");
-        }}
-      >
-        Table
-      </button> */}
+        {activeScreen === "home" && (
+          <div>
+            {/* <center> <h1 style={{color: "rgb(29, 150, 90)"}}>Details</h1></center> */}
+            <div className="source-url">
+              <input
+                type="text"
+                placeholder="SOURCE_URL"
+                value={inputText}
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <div className="dst-url">
+              <input
+                type="text"
+                placeholder="DESTINATION_URL"
+                value={inputText}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="address">
+              <input
+                type="text"
+                placeholder="ADDRESS"
+                value={inputText}
+                onChange={handleInputChange}
+              />
+            </div>
+
+            {/* <div className="submit-button">
+              {activeScreen === "table" && <h1>hello</h1>}
+              <button onClick={handleSubmit}>Submit</button>
+            </div> */}
+            {/* <div className="submit-button"> */}
+
+            {/* <button
+                onClick={() => {
+                  handleChangeActiveScreen("table");
+                }}
+              >
+                submit
+              </button> */}
+            {/* </div> */}
+            <div className="submit-button">
+              <button
+                onClick={() => {
+                  handleChangeActiveScreen("table");
+                }}
+              >
+                submit
+              </button>
+            </div>
+          </div>
+        )}
+
+        {activeScreen === "table" && (
+          <div className="table-size">
+            <h1></h1>
+
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 400 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>
+                      <b>Method</b>
+                    </TableCell>
+                    <TableCell align="right">
+                      <b>From</b>
+                    </TableCell>
+                    <TableCell align="right">
+                      <b>To&nbsp;</b>
+                    </TableCell>
+                    <TableCell align="right">
+                      <b>Hash&nbsp;</b>
+                    </TableCell>
+                    <TableCell align="right">
+                      <b>Timestamp&nbsp;</b>
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow
+                      key={row.method}
+                      sx={{
+                        "&:last-child td, &:last-child th": { border: 0 },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.method}
+                      </TableCell>
+                      <TableCell align="right">{row.from}</TableCell>
+                      <TableCell align="right">{row.to}</TableCell>
+                      <TableCell align="right">{row.hash}</TableCell>
+                      <TableCell align="right">{row.timestamp}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+        )}
+      </div>
 
       {/* <div className="two">
         <h1></h1>
