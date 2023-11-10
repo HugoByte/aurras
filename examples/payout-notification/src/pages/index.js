f95498a88503232fdc042ac71893678188c184dc
Shanith K K <shanith.kk@hugobyte.com>
2023-11-11 04:13:26 +0530

    feat: frontend ui for polkadot payout

---
+++ examples/payout-notification/src/pages/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "../styles.css";
+import SignInForm from "./SignIn";
+import SignUpForm from "./SignUp";
+
+function Home() {
+  const [type, setType] = useState("signIn");
+  const handleOnClick = (text) => {
+    if (text !== type) {
+      setType(text);
+      return;
+    }
+  };
+  const containerClass =
+    "container1 " + (type === "signUp" ? "right-panel-active" : "");
+  return (
+    
+    <div className="App">
+      <h1>Sign in/up Form</h1>
+      <div className={containerClass} id="container">
+        <SignUpForm />
+        <SignInForm />
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
+              <h1>Welcome Back!</h1>
+              <p>
+                To keep connected with us please login with your personal info
+              </p>
+              <button
+                className="ghost"
+                id="signIn"
+                onClick={() => handleOnClick("signIn")}
+              >
+                Sign In
+              </button>
+            </div>
+            <div className="overlay-panel overlay-right">
+              <h1>Hello, Users!</h1>
+              <p>Enter your personal details and start journey with us</p>
+              <button
+                className="ghost "
+                id="signUp"
+                onClick={() => handleOnClick("signUp")}
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
