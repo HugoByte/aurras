92f64b71c505a07aa10314b52e003e324f876dfb
Shanith K K <shanith.kk@hugobyte.com>
2024-03-06 18:39:40 +0400

    chore: update setup script

--- runtime/lite/scripts/ssb-up.sh
+++ runtime/lite/scripts/ssb-up.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 # Function to find a container by name or partially matching name
-
-
-copy-files() {
-  find_container() {
-  local container_name="ssb-pubs"
+find_container() {
+  local container_name=$1
   local running_containers=$(docker ps --format '{{.Names}}')
   for container in $running_containers; do
     if [[ "$container" == "$container_name" || "$container" =~ ^"$container_name".* ]]; then
@@ -17,12 +14,18 @@ copy-files() {
   return 1
 }
 
+copy-files() {
+# Get the container name or pattern to search for
+# read -p "Enter container name (or part of the name): " container_name
 
 # Find the container
 container=$(find_container "ssb-pubs")
 
 # Check if container was found
 if [[ $? -eq 0 ]]; then
+  # Get the command to execute
+#   read -p "Enter the command to execute: " command
+
   # Execute the command inside the container
   echo "$container"
   docker exec -it "$container" bash -c "cp /home/node/config /home/node/.ssb/"
@@ -31,17 +34,14 @@ else
 fi
 }
 
-# Start the Network
 start_service() {
   docker-compose --project-name ssb up -d  
 }
 
-# Stop the Network
 stop_service() {
   docker-compose --project-name ssb down  
 }
 
-# Initilize the file, creating the folder for mounting the config for pub
 initialize() {
   mkdir test
   
@@ -71,6 +71,30 @@ cat > ./test/config <<EOF
 EOF
 }
 
+create_invite(){
+    container=$(find_container "ssb-pubs")
+    invite=$(docker exec -it "$container" bash -c "ssb-server invite.create 2")
+    echo $invite
+
+    # Get the IP address of the Docker container
+    container_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container)
+
+    # Edit the invite with container ip
+    internal_invite=$(echo "$invite" | sed 's/0.0.0.0/'$container_ip/g)
+    echo $internal_invite
+}
+
+accept_invite(){
+    invite=$1
+    consumer_container=$(find_container "ssb-consumer")
+    producer_container=$(find_container "ssb-producer")
+
+    consumer_accept=$(docker exec -it "$consumer_container" bash -c "ssb-server invite.accept $invite")
+    producer_accept=$(docker exec -it "$producer_container" bash -c "ssb-server invite.accept $invite")
+    echo $consumer_accept
+    echo $producer_accept
+}
+
 case "$1" in
   start)
     start_service 
@@ -83,6 +107,12 @@ case "$1" in
     ;;
   init)
     initialize
+    ;;
+  create-invite)
+    create_invite
+    ;;
+  accept-invite)
+    accept_invite $2
     ;;
   *)
     echo "Invalid command. Please enter init, start, copy or stop."
