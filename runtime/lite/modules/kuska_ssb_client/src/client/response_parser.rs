1dcc7876a65ba7b59aedc9b2fc1edc09cc6e135f
Ajay Kumar <87293689+ajaykumargdr@users.noreply.github.com>
2024-03-13 13:54:42 +0530

    feat: Implement SSB client library (#97)
    * chore: add implementation of ssb client library
    
    * chore: remove Cargo.lock file

---
+++ runtime/lite/modules/kuska_ssb_client/src/client/response_parser.rs
@@ -0,0 +1,15 @@
+use super::*;
+
+pub fn whoami_res_parse(body: &[u8]) -> Result<WhoAmIOut> {
+    Ok(serde_json::from_slice(body)?)
+}
+pub fn message_res_parse(body: &[u8]) -> Result<Message> {
+    Ok(Message::from_slice(body)?)
+}
+pub fn feed_res_parse(body: &[u8]) -> Result<Feed> {
+    Ok(Feed::from_slice(&body)?)
+}
+
+pub fn latest_res_parse(body: &[u8]) -> Result<LatestOut> {
+    Ok(serde_json::from_slice(body)?)
+}
