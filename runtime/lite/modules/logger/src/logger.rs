f23bf38159b973ea2f3bd3ecb057ab6376285cf8
Shanith K K <shanith.kk@hugobyte.com>
2024-03-20 13:04:17 +0530

    chore: add log level for lite runtime logger

--- runtime/lite/modules/logger/src/logger.rs
+++ runtime/lite/modules/logger/src/logger.rs
@@ -14,23 +14,30 @@ impl CoreLogger {
     pub fn new(log_file: Option<&str>) -> CoreLogger {
         use std::fs::OpenOptions;
 
-       let file =  match log_file {
-            Some(file) => {
-                OpenOptions::new()
+        let level = std::env::var("LOG_LEVEL").unwrap_or("info".to_string());
+        let level = match level.to_lowercase().as_str() {
+            "info" => slog::Level::Info,
+            "debug" => slog::Level::Debug,
+            "error" => slog::Level::Error,
+            "warn" => slog::Level::Warning,
+            "trace" => slog::Level::Trace,
+            "critical" => slog::Level::Critical,
+            _ => slog::Level::Info,
+        };
+
+        let file = match log_file {
+            Some(file) => OpenOptions::new()
                 .write(true)
                 .append(true)
                 .create(true)
                 .open(file)
-                .unwrap()
-            }
-            None => {
-                OpenOptions::new()
-                    .create(true)
-                    .write(true)
-                    .append(true)
-                    .open("./workflows.log")
-                    .unwrap()
-            }
+                .unwrap(),
+            None => OpenOptions::new()
+                .create(true)
+                .write(true)
+                .append(true)
+                .open("./workflows.log")
+                .unwrap(),
         };
 
         let decorator = slog_term::PlainDecorator::new(file);
@@ -44,6 +51,7 @@ impl CoreLogger {
         let drain = slog_async::Async::new(drain)
             .overflow_strategy(slog_async::OverflowStrategy::Block)
             .build()
+            .filter_level(level)
             .fuse();
         let logger = slog::Logger::root(drain, slog::o!());
 
@@ -66,5 +74,13 @@ impl Logger for CoreLogger {
 
     fn debug(&self, msg: &str) {
         slog::debug!(self.logger, "{msg:?}");
+    }
+
+    fn trace(&self, msg: &str) {
+        slog::trace!(self.logger, "{msg:?}");
+    }
+
+    fn critical(&self, msg: &str) {
+        slog::crit!(self.logger, "{msg:?}");
     }
 }
