bca5c81eb01dc11139ffec19118304996d824f09
ajaykumargdr <ajaykumarncas@gmail.com>
2024-03-28 23:31:29 +0530

    chore: remove event type in ssb client publish event

--- runtime/lite/src/modules/kuska_ssb_client/client/types.rs
+++ runtime/lite/src/modules/kuska_ssb_client/client/types.rs
@@ -1,38 +1,11 @@
 use super::*;
 
-use kuska_ssb::api::dto::content::Mention;
 use serde::{Deserialize, Serialize};
 
 pub struct Client {
     pub api: ApiCaller<TcpStream>,
     pub rpc_reader: RpcReader<TcpStream>,
     pub sk: SecretKey,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Event {
-    pub event: String,
-    pub section: String,
-    pub content: String,
-
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub mentions: Option<Vec<Mention>>,
-}
-
-impl Event {
-    pub fn new(
-        event: String,
-        section: String,
-        content: String,
-        mentions: Option<Vec<Mention>>,
-    ) -> Self {
-        Self {
-            event,
-            section,
-            content,
-            mentions,
-        }
-    }
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
